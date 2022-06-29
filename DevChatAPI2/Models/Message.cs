a58aa89d01d8abd6926a5e38e188cecc2cb21b83
rafaelmoragues <moraguesrafael@gmail.com>
2022-06-29 15:31:52 -0300

    name en message

--- DevChatAPI2/Models/Message.cs
+++ DevChatAPI2/Models/Message.cs
@@ -7,6 +7,7 @@ namespace DevChatAPI2.Models
         [Key]
         public int Id { get; set; }
         public string UserId { get; set; }
+        public string UserName { get; set; }
         public int RoomChatId { get; set; }
         public string MessageBody { get; set; }
         public DateTime Date { get; set; }
--- DevChatAPI2/Request/MessageRequest.cs
+++ DevChatAPI2/Request/MessageRequest.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string UserId { get; set; }
+        public string UserName { get; set; }
         public int RoomChatId { get; set; }
         public string MessageBody { get; set; }
         public DateTime Date { get; set; }
